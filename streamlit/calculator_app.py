099abd7465b56cf1b77b41e0b1d1f55d6707188e
lunafalya <146829371+lunafalya@users.noreply.github.com>
2023-11-02 20:34:15 +0700

    Add files via upload

---
+++ streamlit/calculator_app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import math_operation
+
+st.title("Simple Calculator")
+
+x = st.number_input("Enter the first number:")
+y = st.number_input("Enter the second number:")
+
+operation = st.selectbox("Select an operation",
+                         ("Add", "Subtract", "Multiply", "Divide"))
+
+result = None
+
+if st.button("Calculate"):
+    if operation == "Add":
+        result = math_operation.add(x, y)
+    elif operation == "Subtract":
+        result = math_operation.subtract(x, y)
+    elif operation == "Multiply":
+        result = math_operation.multiply(x, y)
+    elif operation == "Divide":
+        try:
+            result = math_operation.divide(x, y)
+        except ZeroDivisionError:
+            st.error("Division by zero is not allowed.")
+
+if result is not None:
+    st.success(f"Result: {result}")
